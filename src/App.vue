db5e1bd39569c1a57220e1ea1a2e4d115794afd3
Aldi Rahmaddani <aldi.rahmaddani12@gmail.com>
2025-03-08 13:53:08 +0700

    feat: initial commit

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from "vue-router";
+</script>
+
+<template>
+  <header>
+    <nav class="px-4 py-6 flex items-center gap-x-4">
+      <RouterLink class="text-green-500 hover:underline" to="/">Home</RouterLink>
+      <RouterLink class="text-green-500 hover:underline" to="/about">About</RouterLink>
+    </nav>
+  </header>
+  <div class="px-4 py-10">
+    <RouterView />
+  </div>
+</template>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>
+    <p className="text-lg text-blue-500 font-medium font-sans">
+      Hello👋, good luck working on your project 😁
+    </p>
+  </div>
+</template>
